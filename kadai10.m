5a6d2aaa5d4891e6b5038b5c5ca072e6918919e4
mizunoayako <56717821+mizunoayako@users.noreply.github.com>
2019-12-30 23:48:26 +0900

    Update kadai10.m

--- kadai10.m
+++ kadai10.m
@@ -3,22 +3,22 @@
 % 各自，Lenna以外の画像を用いよ． 
 % 例
 
-ORG = imread('Lenna.jpg'); % 原画像の入力
+ORG = imread('adpDSC_2595-750x499.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG); %カラーからグレイへの変換
 imagesc(ORG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
+
 
 IMG = edge(ORG,'prewitt'); % エッジ抽出（プレウィット法）
 imagesc(IMG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
 
+
 IMG = edge(ORG,'sobel'); % エッジ抽出（ソベル法）
 imagesc(IMG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
 
+
 IMG = edge(ORG,'canny'); % エッジ抽出（キャニー法）
 imagesc(IMG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
-
-
-
