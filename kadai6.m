f11fcd110e86f04ebc0bc6f2401285d599a932cb
mizunoayako <56717821+mizunoayako@users.noreply.github.com>
2019-12-30 23:46:05 +0900

    Update kadai6.m

--- kadai6.m
+++ kadai6.m
@@ -4,16 +4,17 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 clear; % 変数のオールクリア
-ORG=imread('Lenna.png'); % 原画像の入力
+ORG=imread('adpDSC_2595-750x499.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG);
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 pause; % 一時停止
+
 
 
 IMG = ORG>128; % 128による二値化
 imagesc(IMG); colormap(gray); colorbar; % 画像の表示
 pause;
 
+
 IMG = dither(ORG); % ディザ法による二値化
 imagesc(IMG); colormap(gray); colorbar; % 画像の表示
-
