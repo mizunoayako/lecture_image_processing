c0601c741a2045a911d6a14e447bcab83fc86ff6
mizunoayako <56717821+mizunoayako@users.noreply.github.com>
2019-12-30 23:47:16 +0900

    Update kadai8.m

--- kadai8.m
+++ kadai8.m
@@ -4,13 +4,13 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ． 
 % 例
 
-ORG = imread('Lenna.jpg'); % 画像の読み込み
+ORG = imread('adpDSC_2595-750x499.jpg'); % 画像の読み込み
 ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 pause;
 IMG = ORG > 128; % 閾値128で二値化
 imagesc(IMG); colormap(gray); colorbar; % 画像の表示
 pause;
-IMG = bwlabeln(IMG);
+IMG = bwlabeln(IMG);   %連結要素をラベル付け
 imagesc(IMG); colormap(jet); colorbar; % 画像の表示
 pause;
